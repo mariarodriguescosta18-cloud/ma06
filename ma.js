857f186804811fca8a7e3303991093e66a0c754c
mariarodriguescosta18-cloud <maria.rodrigues.costa18@escola.pr.gov.br>
2026-08-19 09:42:10 -0300

    Add files via upload

---
+++ ma.js
@@ -0,0 +1,36 @@
+function pegarValores() {
+let n1 = Number(document.getElementById("num1").value);
+let n2 = Number(document.getElementById("num2").value);
+return { n1, n2 };
+
+}
+
+function somar() {
+let valores = pegarValores();
+document.getElementById("resultado").innerText = "Resultado: " +
+(valores.n1 + valores.n2);
+
+}
+
+function subtrair() {
+let valores = pegarValores();
+document.getElementById("resultado").innerText = "Resultado: " +
+(valores.n1 - valores.n2);
+
+
+}
+
+function multiplicar() {
+let valores = pegarValores();
+document.getElementById("resultado").innerText = "Resultado: " +
+(valores.n1 * valores.n2);
+}
+function dividir() {
+let valores = pegarValores();
+if (valores.n2 === 0) {
+docurent.getElenentById("resultado").innerText = "Resultado: aivisac por zero råc permitide";
+} else {
+docunent.getElementById("resultado").innerText = "Resultado: " +
+(valores.n1 / valores.n2);
+}
+ }
